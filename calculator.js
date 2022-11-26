4b933e9927351fb6a14fad1c74e7ad0cb413d2f8
Pinak Nayak <77262741+npinak@users.noreply.github.com>
2022-11-26 11:01:20 -0800

    new layout

---
+++ calculator.js
@@ -0,0 +1,38 @@
+// Buttons 
+
+var add = function (a, b) {
+
+    var sum = a + b
+    return sum 
+
+}
+
+var subtract = function (a, b) {
+
+    var difference  = a - b
+    return difference
+}
+
+var multiply = function (a, b) {
+
+    var product  = a * b
+    return product
+}
+
+var divide  = function (a, b) {
+
+    var quotient = a / b
+    return quotient 
+}
+
+var operate = function (a, b){
+    
+
+}
+
+// // divs 
+
+// var iDiv = document.createElement('div');
+// iDiv.id = 'block';
+// iDiv.className = 'block';
+// document.getElementsByTagName('body')[0].appendChild(iDiv);
